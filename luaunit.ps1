7b002b182b65bd1ed3a9ce0750873d3bbe3769c5
Rizart Dokollari <r.dokollari@gmail.com>
2019-04-14 07:46:58 +0100

    Travis:build luaunit:5.1.

--- luaunit.ps1
+++ luaunit.ps1
@@ -8,19 +8,18 @@
 
 $luaContainer = "luaunit:5.1"
 
-#$luanitInspection = Invoke-Expression "docker image inspect $luaunitTag 2>&1"
+$luanitInspection = Invoke-Expression "docker image inspect $luaunitTag 2>&1"
 
-# Uncomment when work with docker file done
-#If ($luanitInspection -like "*No such image: $luaunitTag*")
-#{
-#Invoke-Expression "docker build --tag=$luaContainer .\tests\"
-#}
+If ($luanitInspection -like "*No such image: $luaunitTag*")
+{
+    Invoke-Expression "docker build --tag=$luaContainer .\tests\"
+}
 
 $dockerRun = "docker run --rm " `
-     + "--user root " `
-     + "--volume $(pwd):/app " `
-     + "--workdir /app " `
-     + "$luaContainer lua tests/TestSuite.lua $args"
+       + "--user root " `
+       + "--volume $( pwd ):/app " `
+       + "--workdir /app " `
+       + "$luaContainer lua tests/TestSuite.lua $args"
 
 Write-Output $dockerRun
 
