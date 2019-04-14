c4746a02090a47acb8c10afe6955e8a9d76886e7
Rizart Dokollari <r.dokollari@gmail.com>
2019-04-14 18:05:04 +0100

    WIP: install.sh: generate sdk from eso API.

--- docker/install.sh
+++ docker/install.sh
@@ -1,11 +1,40 @@
 #!/bin/sh
 
-echo 'test'
+luaunitFile="/app/lib/luaunit.lua"
 
-#curl https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_3/luaunit.lua    \
-#    --output /app/lib/luaunit.lua                                                    \
-#    --silent
+if [[ ! -f ${luaunitFile} ]]; then
+    echo "Installing bluebird75/luaunit..."
+    curl https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_3/luaunit.lua    \
+        --output ${luaunitFile}                                                         \
+        --silent
+else
+    echo "bluebird75/luaunit already installed."
+fi
 
-#git https://github.com/esoui/esoui.git /app/lib/esoui
+esouiZipMaster="/app/lib/esoui-master.zip"
+esouiZipDirectory="/app/lib/esoui-zip"
+esouiDirectory="/app/lib/esoui"
+
+if [[ ! -f ${esouiZipMaster} ]]; then
+    echo "Installing esoui/esoui ..."
+    curl https://codeload.github.com/esoui/esoui/zip/master     \
+    --output ${esouiZipMaster}                                  \
+    --silent
+
+else
+    echo "esoui/esoui already installed."
+fi
+
+echo "In Progress: Unzipping esoui API"
+rm -rf ${esouiDirectory}
+mkdir -p ${esouiDirectory}
+unzip ${esouiZipMaster} -d ${esouiDirectory}
+echo "Completed - Unzipping esoui API"
+
+echo "In Progress: Gathering esoui lua files."
+rm -rf ${esouiZipDirectory}
+mkdir -p ${esouiZipDirectory}
+find ${esouiDirectory} -name "*.lua" -exec mv --force {}  ${esouiZipDirectory} \;
+echo "Complete: Gathering esoui lua files."
 
 
--- readme.md
+++ readme.md
@@ -7,5 +7,11 @@ Storing source inside the system feels like coding on production server :mind_bl
 - Open `cmd` with Admin privileges
 - Paste: `mklink /d "C:\Users\rdok\Documents\Elder Scrolls Online\live\AddOns\Pomodoro" "D:\Code\Pomodoro"`
 
-### Add autocomplete 
-Follow https://www.esoui.com/forums/showthread.php?t=776
+### Issues
+> standard_init_linux.go:207: exec user process caused "no such file or directory"
+Convert file endings to Unix style: 
+- Download http://dos2unix.sourceforge.net/
+- Extract to this directory
+- PowerShell:  `.\dos2unix.exe .\docker\install.sh`
+
+ 
